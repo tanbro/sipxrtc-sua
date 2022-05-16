8a7c07cd6a43af1e3386c354ab2bb365e287cb82
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-05-16 12:19:51 +0800

    change: 改用 SRC_SINC_MEDIUM_QUALITY 作为默认的 resample level

--- src/IpcFlags.cc
+++ src/IpcFlags.cc
@@ -8,19 +8,27 @@
 #include "SipFlags.hh"
 
 static bool valide_path(const char *flagname, const std::string &value);
-static bool valide_resample_level(const char *flagname, uint32_t value);
 
 DEFINE_string(aud_playback_path, "", "向该文件放音(Signed 16-bit PCM Mono)");
 DEFINE_validator(aud_playback_path, &valide_path);
 DEFINE_uint32(aud_playback_samplerate, 48000, "放音的采样率");
 DEFINE_uint32(aud_playback_frametime, 20, "放音的frame时长(milli)");
 DEFINE_uint32(
-    aud_playback_resample_level, SRC_ZERO_ORDER_HOLD,
+    aud_playback_resample_level, SRC_SINC_MEDIUM_QUALITY,
     "放音的重采样(如有必要)级别(0=best/slowest ... 4=poorest/fastest)");
-DEFINE_validator(aud_playback_resample_level, &valide_resample_level);
+DEFINE_validator(aud_playback_resample_level, [](const char *flagname,
+                                                 uint32_t value) {
+  return ((value >= SRC_SINC_BEST_QUALITY) && (value <= SRC_LINEAR));
+});
 
 DEFINE_string(aud_capture_path, "", "从该文件拾音(Signed 16-bit PCM Mono)");
-DEFINE_validator(aud_capture_path, &valide_path);
+DEFINE_validator(aud_capture_path,
+                 [](const char *flagname, const std::string &value) {
+                   if (FLAGS_list_codecs)
+                     return true;
+                   return !value.empty();
+                 });
+
 DEFINE_uint32(aud_capture_samplerate, 48000, "拾音的采样率");
 DEFINE_uint32(aud_capture_frametime, 20, "拾音的frame时长(milli)");
 
@@ -30,8 +38,4 @@ bool valide_path(const char *flagname, const std::string &value) {
   if (FLAGS_list_codecs)
     return true;
   return !value.empty();
-}
-
-bool valide_resample_level(const char *flagname, uint32_t value) {
-  return ((value >= SRC_SINC_BEST_QUALITY) && (value <= SRC_LINEAR));
 }
