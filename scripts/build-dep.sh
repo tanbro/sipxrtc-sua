383ee5f00fb10995257cbd023196a62ca707193e
tanbro <tanbro@163.com>
2024-04-12 08:28:02 +0000

    edit: adjust make script

--- scripts/build-dep.sh
+++ scripts/build-dep.sh
@@ -12,5 +12,7 @@ set -e
 (
     cd submodules/pjproject
     ./configure --disable-video --disable-libyuv --disable-sdl --disable-ffmpeg --disable-v4l2 --disable-openh264 --disable-vpx --disable-libwebrtc --disable-sound
+    echo "export CFLAGS += -fPIC" | tee user.mak
     make dep && make
+    make install
 )
