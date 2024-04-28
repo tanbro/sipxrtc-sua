c3cfb16d271eab71c0aff2884faa26a26716383e
tanbro <tanbro@163.com>
2024-04-28 12:03:04 +0800

    build: 优化 CMake 脚本

--- scripts/build-dep.sh
+++ scripts/build-dep.sh
@@ -12,7 +12,7 @@ set -e
 (
     cd submodules/pjproject
     echo "export CFLAGS += -fPIC" | tee user.mak
-    ./configure --enable-shared --disable-video --disable-libyuv --disable-sdl --disable-ffmpeg --disable-v4l2 --disable-openh264 --disable-vpx --disable-libwebrtc --disable-sound
+    ./configure --disable-video --disable-libyuv --disable-sdl --disable-ffmpeg --disable-v4l2 --disable-openh264 --disable-vpx --disable-libwebrtc --disable-sound
     make dep && make
     make install
 )
